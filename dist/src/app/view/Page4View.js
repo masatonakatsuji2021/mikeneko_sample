20d1792a81f447383713a938bb24de42cb377c98
masato nakatsuji <nakatsuji.sak19@gmail.com>
2024-08-18 16:09:42 +0900

    8/18

--- dist/src/app/view/Page4View.js
+++ dist/src/app/view/Page4View.js
@@ -11,6 +11,9 @@ class Page4View extends View_1.View {
                 value1: this.mjs.value1.value,
                 value2: this.mjs.value2.value,
                 value3: this.mjs.value3.value,
+                value4: this.mjs.value4.value,
+                value5: this.mjs.value5.value,
+                value6: this.mjs.value6.value,
             });
         };
     }
